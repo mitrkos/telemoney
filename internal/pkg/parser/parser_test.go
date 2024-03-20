9252443d38621f775f2c271213ce8b2e882a6a57
mks <mitrkos@gmail.com>
2024-03-20 13:25:30 +0200

    brake test to check ci

--- internal/pkg/parser/parser_test.go
+++ internal/pkg/parser/parser_test.go
@@ -113,7 +113,7 @@ func TestParser_ParseTransactionUserInputDataFromTextSuccess(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			result, err := p.ParseTransactionUserInputDataFromText(tc.text)
-			require.NoError(t, err)
+			require.Error(t, err)
 			require.Equal(t, tc.expectedResult, result, "TransactionUserInputDatas aren't equal")
 		})
 	}
